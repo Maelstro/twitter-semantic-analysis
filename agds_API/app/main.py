2f4418f91eab5ab1425455557f499bd1fa17fe2d
Maciej Wójcik <maciej_wojcik@ymail.com>
2021-05-10 21:44:08 +0200

    [Add] Add static and templates directory to API, example endpoints

--- agds_API/app/main.py
+++ agds_API/app/main.py
@@ -1,1 +1,20 @@
 # main.py - main file of the application
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+# Create AGDS FastAPI instance
+agds_api = FastAPI()
+
+# Mount static and template directory
+agds_api.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@agds_api.get("/")
+@agds_api.get("/hello")
+async def hello():
+    return "Hello World!"
+
+@agds_api.post("/associate")
+async def text_association(input_text: str) -> dict:
+    return {}
