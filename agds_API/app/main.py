f4a8fed5fef9041085d0bfa73f46562b3b18b7b2
Maciej Wojcik <maciej_wojcik@ymail.com>
2021-05-30 01:02:05 +0200

    Finished website

--- agds_API/app/main.py
+++ agds_API/app/main.py
@@ -5,6 +5,12 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from text_processing.text_association import TextProcessor
 from starlette.exceptions import HTTPException as StarletteHTTPException
+import asyncio
+import json
+import aioredis
+import pandas as pd
+
+redis = aioredis.from_url("redis://agds-redis")
 
 # Create AGDS FastAPI instance
 agds_api = FastAPI()
@@ -18,6 +24,8 @@ templates = Jinja2Templates(directory="templates")
 
 @agds_api.get("/", response_class=HTMLResponse)
 async def main_page(request: Request):
+    x = await redis.keys('prefix:*')
+    for key in x: redis.delete(key)
     return templates.TemplateResponse(name="index.html", context={"request": request})
 
 @agds_api.get("/hello")
@@ -28,25 +36,86 @@ async def hello():
 async def text_association(request: Request, input_text: str = Form(...)) -> templates.TemplateResponse:
     # Calculate text similarity
     trait_result, influencer_list = text_proc.calculate_similarity(input_text)
+    trait_result = trait_result.to_dict()
+    await redis.set("agds_traits", json.dumps(trait_result))
+    await redis.set("agds_most_similar", json.dumps(influencer_list.to_dict()))
+
+    # Neural network processing
+    text_pred = text_proc.predict_nn(input_text)
+    nn_similarity = text_proc.nn_similarity(text_pred)
+    text_pred = text_pred.to_dict()
+
+    await redis.set("nn_traits", json.dumps(text_pred))
+    await redis.set("nn_most_similar", json.dumps(nn_similarity.to_dict()))
 
     # Return trait outputs and list of ten most similar influencers
     return templates.TemplateResponse(name="process.html", context={
         "request": request,
         "input_text": input_text,
-        "traits": trait_result.to_dict(),
-        "most_similar_list": influencer_list[:10]
+        "traits": trait_result,
+        "most_similar_list": influencer_list[:10],
+        "model_type": "AGDS"
     })
 
 @agds_api.post("/nn_associate", response_class=HTMLResponse)
 async def nn_association(request: Request, input_text: str = Form(...)) -> templates.TemplateResponse:
+    trait_result = await redis.get("nn_traits")
+    influencer_list = await redis.get("nn_most_similar")
+    influencer_list = pd.Series(json.loads(influencer_list))
+
     return templates.TemplateResponse(name="process.html", context={
         "request": request,
         "input_text": input_text,
-        "traits": {}
+        "traits": json.loads(trait_result),
+        "most_similar_list": influencer_list[:10],
+        "model_type": "LSTM"
     })
+
+@agds_api.post("/agds_associate", response_class=HTMLResponse)
+async def agds_association(request: Request, input_text: str = Form(...)) -> templates.TemplateResponse:
+    trait_result = await redis.get("agds_traits")
+    influencer_list = await redis.get("agds_most_similar")
+    influencer_list = pd.Series(json.loads(influencer_list))
+
+    return templates.TemplateResponse(name="process.html", context={
+        "request": request,
+        "input_text": input_text,
+        "traits": json.loads(trait_result),
+        "most_similar_list": influencer_list[:10],
+        "model_type": "AGDS"
+    })
+
+@agds_api.post("/compare", response_class=HTMLResponse)
+async def compare_results(request: Request, input_text: str = Form(...)) -> templates.TemplateResponse:
+    agds_trait_result = await redis.get("agds_traits")
+    agds_influencer_list = await redis.get("agds_most_similar")
+    agds_influencer_list = json.loads(agds_influencer_list)
+
+    nn_trait_result = await redis.get("nn_traits")
+    nn_influencer_list = await redis.get("nn_most_similar")
+    nn_influencer_list = json.loads(nn_influencer_list)
+
+    # Create lists
+    l_inf = [agds_influencer_list, nn_influencer_list]
+    l_tr = [json.loads(agds_trait_result), json.loads(nn_trait_result)]
+
+    both_influencer_list = list(zip(*map(dict.items, l_inf)))
+    both_traits = list(zip(*map(dict.items, l_tr)))
+
+    return templates.TemplateResponse(name="compare.html", context={
+        "request": request,
+        "input_text": input_text,
+        "both_traits": both_traits,
+        "both_most_similar_list": both_influencer_list[:10],
+    })
+
 
 @agds_api.exception_handler(StarletteHTTPException)
 async def http_exception(request: Request, exc: StarletteHTTPException):
     return templates.TemplateResponse(name="40X.html", context={
         "request": request,
     }, status_code=exc.status_code)
+
+@agds_api.on_event("shutdown")
+async def shutdown_event():
+    redis.close()
--- agds_API/app/text_processing/text_association.py
+++ agds_API/app/text_processing/text_association.py
@@ -6,16 +6,29 @@ import re
 from collections import Counter
 import numpy as np
 import pickle
+from scipy.special import softmax
+import tensorflow as tf
 
 class TextProcessor(object):
     def __init__(self):
         # Load the required files
-        with open("text_processing/word_map_non_negative.pickle", "rb") as f:
-            self.word_map = pickle.load(f)
-
-        with open("text_processing/word_trait_array_non_negative.pickle", "rb") as f:
+        with open("text_processing/finetuned_s90_10_word_trait_array.pickle", "rb") as f:
             self.word_df = pickle.load(f)
-        self.arch_df = pd.read_csv("text_processing/influencer_recalc.csv", header=0, index_col=0)
+
+        # Generate word map from AGDS
+        self.word_map = self.word_df.columns.tolist()
+
+        # Read archetype list and clean it up
+        self.arch_df = pd.read_csv("text_processing/archetypes_pl_new.csv", header=0, index_col=0)
+        self.arch_df = self.arch_df.fillna(2)
+        self.arch_df = self.arch_df[~self.arch_df.index.duplicated(keep='first')]
+
+        # Generate trait list
+        self.trait_list = self.arch_df.columns.tolist()
+
+        # Load LSTM model
+        self.test_model = tf.keras.models.load_model("text_processing/nn_model")
+
 
     def extract_hashtags(self, post_text):
         HASH_RE = re.compile(r"\#\w+")
@@ -39,7 +52,24 @@ class TextProcessor(object):
 
         # Calculate dot product for a given text
         word_dot = self.word_df.dot(post_vector)
-        return word_dot
+
+        out_vec = pd.Series()
+        for trait in self.trait_list:
+            out_vec = out_vec.append(pd.Series([np.argmax(softmax(word_dot.loc[trait]))], index=[trait]))
+
+        return out_vec
+
+    # Trait accuracy - round the results
+    def natural_round(x: float) -> int:
+        out = int(x // 1)
+        return out + 1 if (x - out) >= 0.5 else out
+
+    def accuracy_per_trait(input_vector: pd.Series, annotated_vector: pd.Series) -> np.array:
+        out_array = np.array([0] * 37, dtype=np.int)
+        for i in range(len(out_array)):
+            if input_vector[i] == annotated_vector[i]:
+                out_array[i] = 1
+        return out_array
 
     # Method for calculating the similarity
     def calculate_similarity(self, post_text: str) -> (pd.Series, pd.Series):
@@ -55,3 +85,37 @@ class TextProcessor(object):
 
         sorted_infs = inf_df.sort_values()
         return post_result, sorted_infs
+
+    def clean_post(self, src_text: str) -> str:
+        # Extract posts and hashtags
+        extracted_text = remove_stopwords(clean_up_text(src_text))
+        extracted_hashtags = self.extract_hashtags(src_text)
+        return extracted_text + extracted_hashtags
+
+    def predict_nn(self, post_text):
+        # Preprocess the text
+        user_text = " ".join(self.clean_post(post_text))
+
+        # Make a prediction
+        prediction = self.test_model.predict([user_text])
+
+        # Process the predictions
+        predicted_classes = []
+        for trait in prediction:
+            predicted_classes.append(int(np.argmax(trait)))
+
+        predicted_dict = {trait: pred for trait, pred in zip(self.trait_list, predicted_classes)}
+        series_pred = pd.Series(predicted_dict)
+
+        return series_pred
+
+    def nn_similarity(self, predicted_classes):
+        # Generate new dataframe - one row per influencer
+        inf_df = pd.Series(index=self.arch_df.index)
+
+        # Replace all data in temporary df with calculated post result
+        for idx in inf_df.index:
+            inf_df.loc[idx] = np.linalg.norm(self.arch_df.loc[idx] - predicted_classes)
+
+        sorted_infs = inf_df.sort_values()
+        return sorted_infs
